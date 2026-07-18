3647c65b4819841e963c16813c0245332812b476
krish dhiman <krishdhiman86@gmail.com>
2026-07-18 21:21:23 +0530

    new project

--- src/com/attendance/Student.java
+++ src/com/attendance/Student.java
@@ -1,4 +1,28 @@
 package com.attendance;
 
 public class Student {
+
+    private int id;
+    private String name;
+    private String course;
+
+    // Constructor
+    public Student(int id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
 }
--- src/com/attendance/User.java
+++ src/com/attendance/User.java
@@ -1,4 +1,22 @@
 package com.attendance;
 
 public class User {
+
+    private String username;
+    private String password;
+
+    // Constructor
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
