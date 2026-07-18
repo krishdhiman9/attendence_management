147d58e7633360abdcaa1ed2ec82d27d52ce3914
krish dhiman <krishdhiman86@gmail.com>
2026-07-18 14:06:55 +0530

    new project

--- src/com/attendance/AttendanceManager.java
+++ src/com/attendance/AttendanceManager.java
@@ -1,4 +1,33 @@
 package com.attendance;
 
+import java.util.ArrayList;
+
 public class AttendanceManager {
+
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+
+        students.add(student);
+
+        System.out.println("Student Added Successfully!");
+    }
+
+    public void viewStudents() {
+
+        if (students.isEmpty()) {
+            System.out.println("No Students Found!");
+            return;
+        }
+
+        System.out.println("\n===== Student List =====");
+
+        for (Student s : students) {
+
+            System.out.println("ID      : " + s.getId());
+            System.out.println("Name    : " + s.getName());
+            System.out.println("Course  : " + s.getCourse());
+            System.out.println("------------------------");
+        }
+    }
 }
